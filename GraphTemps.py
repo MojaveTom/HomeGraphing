5eb49f4a310746c94a8e105a03ff10749a8f1ebe
tom <tom@demayfamly.net>
2019-02-08 11:17:07 -0800

    Define 'log_file_path' in loggingconf and process it in logger setup.

--- GraphTemps.py
+++ GraphTemps.py
@@ -27,9 +27,18 @@ if os.path.isfile(logConfFileName):
     try:
         with open(logConfFileName, 'r') as logging_configuration_file:
             config_dict = json.load(logging_configuration_file)
+        if 'log_file_path' in config_dict:
+            logPath = os.path.expandvars(config_dict['log_file_path'])
+            os.makedirs(logPath, exist_ok=True)
+        else:
+            logPath=""
+        for p in config_dict['handlers'].keys():
+            if 'filename' in config_dict['handlers'][p]:
+                config_dict['handlers'][p]['filename'] = os.path.join(logPath, config_dict['handlers'][p]['filename'])
         logging.config.dictConfig(config_dict)
-    except:
+    except Exception as e:
         print("loading logger config from file failed.")
+        print(e)
         pass
 
 logger = logging.getLogger(__name__)
@@ -313,6 +322,17 @@ def ShowRCLaundry(DBConn):
         data.plot(ax=ax1)
     else:
         logger.info("No data to plot")
+
+    logger.info("          ----------  OUTSIDE  ----------")
+    query = "SELECT {timeField} AS 'Time', OutsideTemp AS 'Outside' \
+        FROM `{schema}`.`weather` WHERE {timeField} > '%s' ORDER BY {timeField}".format(timeField='Time', schema=myschema)
+    logger.info("Out Temp SQL: %s", query)
+    data = GetData(DBConn, 'RcOutTemps.csv', query, beginDate, dataTimeOffsetUTC=ServerTimeFromUTC)
+    if len(data) > 0:
+        data.plot(ax=ax1)
+    else:
+        logger.info("No data to plot")
+
     ax1.set_xlim(left=BeginTime, right=(datetime.utcnow() + ServerTimeFromUTC))
     plt.show()
     plt.close(fig)
